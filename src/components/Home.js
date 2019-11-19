69d7ce96cea8169685b6e4a2ebda61e9171aa110
Amir Mustafa <amirengg15@gmail.com>
2019-11-19 07:35:54 +0530

    Added react router functionality

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="homestyle">
+      <h2>This is the Home page</h2>
+    </div>
+  );
+};
+
+export default Home;
